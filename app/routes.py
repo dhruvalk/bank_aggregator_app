1935a04ce16fd8a05140f5e2a6bceeba32817f80
dhruvalk <kothari.dhruval@gmail.com>
2024-04-12 21:57:43 +0800

    completed upload file ui

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,24 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 
-@app.route('/')
+cache = []
+names = {'DBS':'', 'Paylah!':'', 'POSB':'', 'OCBC':''}
+
+@app.route('/',methods=['GET', 'POST'])
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Home')
+    if request.method == 'POST':
+        bank = request.args.get('bank')
+        print(bank)
+        file = request.files['file']
+        print(request.files)
+        file_name = file.filename
+        names[bank] = file_name
+        cache.append(file)
+        
+    return render_template('index.html', title='Home', banks=['DBS',"Paylah!","POSB","OCBC"], names=names )
 
+@app.route('/files', methods=['GET', 'POST'])
+def files():
+    print(cache)
+    return "Look at the python Flask terminal for the files uploaded."
