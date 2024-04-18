29958b4b948e3e3cb51460ac75f72c21e63d85ae
dhruvalk <kothari.dhruval@gmail.com>
2024-04-18 12:34:08 +0800

    set up chartjs

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,7 @@ from io import BytesIO
 cache = {'DBS':None, 'Paylah!':None, 'POSB':None, 'UOB':None, 'HSBC':None}
 names = {'DBS':'', 'Paylah!':'', 'POSB':'', 'UOB':'', 'HSBC':''}
 banks = ['DBS',"Paylah!","POSB","UOB","HSBC"]
+dfs = {'in_df':pd.DataFrame(), 'out_df':pd.DataFrame()}
 
 @app.route('/',methods=['GET', 'POST'])
 @app.route('/index')
@@ -23,9 +24,24 @@ def index():
         # cache[bank]=file
     return render_template('index.html', title='Home', banks=banks, names=names )
 
-@app.route('/files', methods=['GET', 'POST'])
-def files():
+@app.route('/summary', methods=['GET', 'POST'])
+def summary():
     columns = ['Date','Description','Amount']
+    labels = "<>".join([
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+    ])
+ 
+    data = [0, 10, 15, 8, 22, 18, 25]
+    
+
+    if len(dfs['in_df']) != 0 and len(dfs['out_df']) != 0:
+        return render_template("summaryTable.html",title='Summary',labels=labels, data=data )
+
     in_df = pd.DataFrame(columns=columns)
     out_df = pd.DataFrame(columns=columns)
     for key,file in cache.items():
@@ -40,7 +56,36 @@ def files():
                 in_df, out_df = append_paylah_data(file, in_df, out_df)
             elif key == "HSBC":
                 in_df, out_df = append_hsbc_data(file,in_df,out_df)
-    return out_df.to_html() + in_df.to_html()
+    dfs['in_df'] = in_df
+    dfs['out_df'] = out_df
+    return render_template("summaryTable.html",title='Summary', labels=labels, data=data)
+
+@app.route('/breakdown', methods=['GET', 'POST'])
+def breakdown():
+    columns = ['Date','Description','Amount']
+
+    if len(dfs['in_df']) != 0 and len(dfs['out_df']) != 0:
+        return render_template("breakdownTable.html",title='Breakdown', in_df=dfs['in_df'], out_df=dfs['out_df'])
+
+    in_df = pd.DataFrame(columns=columns)
+    out_df = pd.DataFrame(columns=columns)
+    for key,file in cache.items():
+        if file is not None:
+            if key == "POSB":
+                in_df, out_df = append_posb_data(file, in_df, out_df)
+            elif key == "DBS":
+                in_df, out_df = append_dbs_data(file, in_df, out_df)
+            elif key == "UOB":
+                in_df, out_df = append_uob_data(file, in_df, out_df)
+            elif key == "Paylah!":
+                in_df, out_df = append_paylah_data(file, in_df, out_df)
+            elif key == "HSBC":
+                in_df, out_df = append_hsbc_data(file,in_df,out_df)
+    dfs['in_df'] = in_df
+    dfs['out_df'] = out_df
+    return render_template("breakdownTable.html",title='Breakdown', in_df=in_df, out_df=out_df)
+
+
     
 @app.route('/clear', methods=['GET', 'POST'])
 def clear():
